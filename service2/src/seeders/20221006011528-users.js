ff51ef890d218f4dd167822b601223e3fdc7f856
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2022-10-06 11:06:50 +0800

    Update hashPassword on seeder

--- service2/src/seeders/20221006011528-users.js
+++ service2/src/seeders/20221006011528-users.js
@@ -1,11 +1,14 @@
 "use strict";
 
+const { hashPassword } = require("../helper/bcrypt");
+
 /*@type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     let users = require("./db.json").users.map((user) => {
       user.createdAt = new Date();
       user.updatedAt = new Date();
+      user.password = hashPassword(user.password);
       return user;
     });
     await queryInterface.bulkInsert("Users", users, {});
